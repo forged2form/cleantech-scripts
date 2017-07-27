4ecd5f2a942c7e836589ea8bb3d14b33a829cfee
Forged2Form <conrad.tim@gmail.com>
2017-07-27 19:51:59 -0400

    Fix Boot Timer scaper

--- autoclean-startclean.bat
+++ autoclean-startclean.bat
@@ -68,8 +68,8 @@ color 4f
 		pause
 		%workingdir%/nircmd/nircmd win settopmost title "CleanTech: Boottimer" 1
 		echo Grabbing number from dialog box...
-		echo Command running: %workingdir%\sysexp.exe /process BootTimer.exe %workingdir%\%1-%2-%3-BootTimer.txt
-		%workingdir%\sysexp.exe /process boottimer.exe /stext %workingdir%\%1-%2-%3-BootTimer.txt
+		echo Command running: %workingdir%\sysexp.exe /title "WINDOWS BOOT TIME UTILTY" %workingdir%\%1-%2-%3-BootTimer.txt
+		%workingdir%\sysexp.exe /title "WINDOWS BOOT TIME UTILITY" /stext %workingdir%\%1-%2-%3-BootTimer.txt
 		echo,
 		pause
 		taskkill /im BootTimer.exe /t
