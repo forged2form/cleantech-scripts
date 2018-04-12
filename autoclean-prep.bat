de232e7d5b6c21a3dd9cf07e41dcf6942e97bc9c
forged2form <conrad.tim@gmail.com>
2018-04-12 11:23:50 -0400

    Fixed choco install issue...

--- autoclean-prep.bat
+++ autoclean-prep.bat
@@ -289,7 +289,7 @@ if '%errorlevel%' NEQ '0' (
 
 	:chocoinstall
 	echo installing Chocolatey
-         @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).Downloa    dString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+         @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 	:systeminfo
 		echo Dumping preclean system info...
