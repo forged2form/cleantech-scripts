a242551ef463b69f7d5157d93cc70ad1399fae8b
FAHT <conrad.tim@gmail.com>
2019-04-09 16:55:31 -0400

    Automating the "A"...

--- fahtdiag.sh
+++ fahtdiag.sh
@@ -3,11 +3,25 @@
 ####### TechTutors Diag Script ########
 ####### For internal use only! ########
 
-FIRSTNAME=
-LASTNAME=
-CONFIRM=n
-CLIENTNAME=
-DATE=`date +%Y-%m-%d-%H`
+FAHT_FIRSTNAME=
+FAHT_LASTNAME=
+FAHT_CONFIRM=n
+FAHT_CLIENTNAME=
+FAHT_DATE=`date +%Y-%m-%d-%H`
+FAHT_WORKINGDIR=/home/$(whoami)
+FAHT_MACHINE=$(dmidecode|grep "Product Name:"|sed 's/.*Product Name: //')
+FAHT_CORE_COUNT=$(dmidecode|grep "Socket Designation: CPU "|sed 's/[^0-9]*//g'|tail -1)
+FAHT_CORE_COUNT=$(($FAHT_CORE_COUNT +1))
+FAHT_CORE_THREAD=$(dmidecode|grep -m 1 "Thread Count:"|sed 's/[^0-9]*//g')
+FAHT_MEMORY_GB=$(dmidecode|grep -m 1 "Maximum Capacity:"|sed 's/[^0-9]*//g')
+FAHT_TOTAL_THREADS=$(($FAHT_CORE_COUNT * $FAHT_CORE_THREAD))
+FAHT_CPU_MODEL=$(cat /proc/cpuinfo|grep -m 1 "model name"|sed -r 's/model name.*: (.*)/\1/g'|sed -n 's/  */ /gp')
+
+### Note block device where linux is currently mounted for using as an exception when listing hdds
+FAHT_LIVE_DEV=$(mount|grep "on / "|sed -n 's/^\/dev\/\([a-z][a-z][a-z]\).*/\1/gp')
+
+set -o posix ;set|grep FAHT|less
+#>$FAHT_WORKINGDIR/sysinfo.txt
 
 clear
 echo -e "---------------------------"
@@ -31,6 +45,8 @@ while true; do
 		*) echo -e "Please retry... \n";;
 	esac
 done
+
+while
 
 CLIENTNAME=$LASTNAME-$FIRSTNAME
 
