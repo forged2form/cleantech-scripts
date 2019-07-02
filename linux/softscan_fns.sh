40db3a6821c8f8c217ed43e19f8d64f7be6c5055
Tim Conrad <tconrad@tt1.local>
2019-07-02 15:27:24 -0400

    Added quickmode for clamscan

--- linux/softscan_fns.sh
+++ linux/softscan_fns.sh
@@ -5,16 +5,41 @@
 
 virus_scan () {
 
-	echo "------------------------------------------"
-	echo "Scanning for viruses, malware, and PUPs..."
-	echo "------------------------------------------"
-	echo
+	if [ "${FAHT_CLAMAV}" == "true" ] ; then
 
-	echo Mounting volumes to scan....
-	echo
-	mount_avail_volumes
+		if [ "${FAHT_QUICKMODE}" =="true" ]; then
 
-	echo
-	echo Begnning scan...
-	clamscan --bell --detect-pua=yes -irl "${FAHT_WORKINGDIR}/clamscan.txt" /mnt/faht/*
+			echo "------------------------------------------------------"
+			echo "Scanning for viruses, malware, and PUPs (QUICKMODE)..."
+			echo "------------------------------------------------------"
+			echo
+
+			echo Mounting volumes to scan....
+			echo
+			mount_avail_volumes
+
+			echo
+			echo Beginning scan...
+			clamscan --bell -irl "${FAHT_WORKINGDIR}/clamscan.txt" /mnt/faht/*
+		fi
+		
+		if [ "${FAHT_QUICKMODE}" != "true" ] ; then
+
+			echo "------------------------------------------"
+			echo "Scanning for viruses, malware, and PUPs..."
+			echo "------------------------------------------"
+			echo
+
+			echo Mounting volumes to scan....
+			echo
+			mount_avail_volumes
+
+			for curr_dir in "Windows Users" ; do
+				echo
+				echo Beginning scani of ${curr_dir}...
+				clamscan --bell -irl "${FAHT_WORKINGDIR}/clamscan-${curr_dir}.txt" /mnt/faht/${FAHT_WIN_PART}/${curr_dir}/
+			done
+			echo
+		fi
+	fi
 }
