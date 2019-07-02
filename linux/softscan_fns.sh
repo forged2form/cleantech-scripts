6e7ec62c063d2408b82877fa68471fd18f215c17
Tim Conrad <tconrad@tt1.local>
2019-07-02 15:40:59 -0400

    More clamscan refinement

--- linux/softscan_fns.sh
+++ linux/softscan_fns.sh
@@ -21,6 +21,20 @@ virus_scan () {
 			echo
 			echo Beginning scan...
 			clamscan --bell -irl "${FAHT_WORKINGDIR}/clamscan.txt" /mnt/faht/*
+
+			FAHT_CLAMSCAN_RESULTS="$(echo $?)"
+
+			if [ "$FAHT_CLAMSCAN_RESULTS" eq "1" ] ; then
+				FAHT_PUPS="ALERT!!! MALICIOUS SOFTWARE DETECTED!!!i"
+				echo "$FAHT_PUPS"
+				FAHT_PUPS_RESULTS="FAILED"
+			fi
+			
+			if [ "$FAHT_CLAMSCAN_RESULTS" eq "0" ]  && [ "${FAHT_PUPS_RESULTS}" !="FAILED" ]; then
+				FAHT_PUPS="No malicious software detected."
+				echo "$FAHT_PUPS"
+				FAHT_PUPS_RESULTS="PASSED"
+			fi
 		fi
 		
 		if [ "${FAHT_QUICKMODE}" != "true" ] ; then
@@ -38,6 +52,19 @@ virus_scan () {
 				echo
 				echo Beginning scani of ${curr_dir}...
 				clamscan --bell -irl "${FAHT_WORKINGDIR}/clamscan-${curr_dir}.txt" /mnt/faht/${FAHT_WIN_PART}/${curr_dir}/
+				FAHT_CLAMSCAN_RESULTS="$(echo $?)"
+
+				if [ "$FAHT_CLAMSCAN_RESULTS" eq "1" ] ; then
+					FAHT_PUPS="ALERT!!! MALICIOUS SOFTWARE DETECTED!!!i"
+					echo "$FAHT_PUPS"
+					FAHT_PUPS_RESULTS="FAILED"
+				fi
+				
+				if [ "$FAHT_CLAMSCAN_RESULTS" eq "0" ]  && [ "${FAHT_PUPS_RESULTS}" !="FAILED" ]; then
+					FAHT_PUPS="No malicious software detected."
+					echo "$FAHT_PUPS"
+					FAHT_PUPS_RESULTS="PASSED"
+				fi
 			done
 			echo
 		fi
