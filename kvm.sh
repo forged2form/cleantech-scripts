96ef2e0aa5536c562aa9b880318dd469e70a875d
FAHT <conrad.tim@gmail.com>
2019-07-02 11:10:19 -0400

    Beginnings of vm stage

--- kvm.sh
+++ kvm.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-sudo kvm -m 2048 -drive if=ide,media=disk,format=raw,file=/dev/sda -drive if=ide,media=cdrom,file=/root/WinPE_amd64.iso -boot order=dc
+### Launch a KVM instance on disk with detected windows partition.
+### Idea is to have a WinPE ISO that will launch a batch script that will run chkdsk scan (among others)
+### that will then save to the USBDATA volume under the current FAHT working directory.
+### Pseudo code for now.
+### Should be run elevated, and called by fahtdiag.
 
+## launch_kvm $VAR (disk to set as first HDD in vm)
+
+launch_kvm () {
+
+	kvm -m 2048 -drive if=ide,media=disk,format=raw,file=${CHANGEME_DETECTED_WIN_DISK} -drive if=ide,media=cdrom,file=/usr/share/faht/WinPE_amd64.iso -boot order=dc
+}
+
+## launch_qmeu $VAR (disk to set as first HDD)
+
+launch_qemu () {
+	qemu-system-x86_64 -m 1024 -drive if=ide,media=disk,format=raw,file=${CHANGEME_DETECTED_WIN_DISK} -drive if=ide,media=cdrom,file=/usr/share/faht/WinPE_amd64.iso -boot order=dc
+
+}
